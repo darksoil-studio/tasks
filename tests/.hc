a0c61fe6fb5288693e640dcdea4bc2191e155ad0
guillem.cordoba <guillem.cordoba@gmail.com>
2024-07-05 16:51:06 +0200

    Removed invalid test

---
+++ tests/.hc
@@ -0,0 +1,2 @@
+/tmp/lsc8PFbP1xZt5ddPTSm_W
+/tmp/IuI3LdWJt6MxZ1Pt-Unkj
--- tests/src/task.test.ts
+++ tests/src/task.test.ts
@@ -1,112 +1,76 @@
-import { assert, test } from "vitest";
-
-import { runScenario, dhtSync } from '@holochain/tryorama';
-import { ActionHash, SignedActionHashed, Delete, Record } from '@holochain/client';
-import { decode } from '@msgpack/msgpack';
+import { toPromise } from '@holochain-open-dev/signals';
 import { EntryRecord } from '@holochain-open-dev/utils';
 import { cleanNodeDecoding } from '@holochain-open-dev/utils/dist/clean-node-decoding.js';
-import { toPromise } from '@holochain-open-dev/signals';
+import {
+	ActionHash,
+	Delete,
+	Record,
+	SignedActionHashed,
+} from '@holochain/client';
+import { dhtSync, runScenario } from '@holochain/tryorama';
+import { decode } from '@msgpack/msgpack';
+import { assert, test } from 'vitest';
 
-import { Task } from '../../ui/src/types.js';
 import { sampleTask } from '../../ui/src/mocks.js';
+import { Task } from '../../ui/src/types.js';
 import { setup } from './setup.js';
 
 test('create Task', async () => {
-  await runScenario(async scenario => {
-    const { alice, bob } = await setup(scenario);
+	await runScenario(async scenario => {
+		const { alice, bob } = await setup(scenario);
 
-    // Alice creates a Task
-    const task: EntryRecord<Task> = await alice.store.client.createTask(await sampleTask(alice.store.client));
-    assert.ok(task);
-  });
+		// Alice creates a Task
+		const task: EntryRecord<Task> = await alice.store.client.createTask(
+			await sampleTask(alice.store.client),
+		);
+		assert.ok(task);
+	});
 });
 
 test('create and read Task', async () => {
-  await runScenario(async scenario => {
-    const { alice, bob } = await setup(scenario);
+	await runScenario(async scenario => {
+		const { alice, bob } = await setup(scenario);
 
-    const sample = await sampleTask(alice.store.client);
+		const sample = await sampleTask(alice.store.client);
 
-    // Alice creates a Task
-    const task: EntryRecord<Task> = await alice.store.client.createTask(sample);
-    assert.ok(task);
+		// Alice creates a Task
+		const task: EntryRecord<Task> = await alice.store.client.createTask(sample);
+		assert.ok(task);
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0]
-    );
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
 
-    // Bob gets the created Task
-    const createReadOutput: EntryRecord<Task> = await toPromise(bob.store.tasks.get(task.actionHash).original);
-    assert.deepEqual(sample, cleanNodeDecoding(createReadOutput.entry));
-  });
-});
-
-test('create and update Task', async () => {
-  await runScenario(async scenario => {
-    const { alice, bob } = await setup(scenario);
-
-    // Alice creates a Task
-    const task: EntryRecord<Task> = await alice.store.client.createTask(await sampleTask(alice.store.client));
-    assert.ok(task);
-        
-    const originalActionHash = task.actionHash;
- 
-    // Alice updates the Task
-    let contentUpdate = await sampleTask(alice.store.client);
-
-    let updatedTask: EntryRecord<Task> = await alice.store.client.updateTask(originalActionHash, originalActionHash, contentUpdate);
-    assert.ok(updatedTask);
-
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0]
-    );
-        
-    // Bob gets the updated Task
-    const readUpdatedOutput0: EntryRecord<Task> = await toPromise(bob.store.tasks.get(task.actionHash).latestVersion);
-    assert.deepEqual(contentUpdate, cleanNodeDecoding(readUpdatedOutput0.entry));
-
-    // Alice updates the Task again
-    contentUpdate = await sampleTask(alice.store.client);
-
-    updatedTask = await alice.store.client.updateTask(originalActionHash, updatedTask.actionHash, contentUpdate);
-    assert.ok(updatedTask);
-
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0]
-    );
-        
-    // Bob gets the updated Task
-    const readUpdatedOutput1: EntryRecord<Task> = await toPromise(bob.store.tasks.get(originalActionHash).latestVersion);
-    assert.deepEqual(contentUpdate, cleanNodeDecoding(readUpdatedOutput1.entry));
-  });
+		// Bob gets the created Task
+		const createReadOutput: EntryRecord<Task> = await toPromise(
+			bob.store.tasks.get(task.actionHash).original,
+		);
+		assert.deepEqual(sample, cleanNodeDecoding(createReadOutput.entry));
+	});
 });
 
 test('create and delete Task', async () => {
-  await runScenario(async scenario => {
-    const { alice, bob } = await setup(scenario);
+	await runScenario(async scenario => {
+		const { alice, bob } = await setup(scenario);
 
-    // Alice creates a Task
-    const task: EntryRecord<Task> = await alice.store.client.createTask(await sampleTask(alice.store.client));
-    assert.ok(task);
-        
-    // Alice deletes the Task
-    const deleteActionHash = await alice.store.client.deleteTask(task.actionHash);
-    assert.ok(deleteActionHash);
+		// Alice creates a Task
+		const task: EntryRecord<Task> = await alice.store.client.createTask(
+			await sampleTask(alice.store.client),
+		);
+		assert.ok(task);
 
-    // Wait for the created entry to be propagated to the other node.
-    await dhtSync(
-      [alice.player, bob.player],
-      alice.player.cells[0].cell_id[0]
-    );
-        
-    // Bob tries to get the deleted Task
-    const deletes: Array<SignedActionHashed<Delete>> = await toPromise(bob.store.tasks.get(task.actionHash).deletes);
-    assert.equal(deletes.length, 1);
-  });
+		// Alice deletes the Task
+		const deleteActionHash = await alice.store.client.deleteTask(
+			task.actionHash,
+		);
+		assert.ok(deleteActionHash);
+
+		// Wait for the created entry to be propagated to the other node.
+		await dhtSync([alice.player, bob.player], alice.player.cells[0].cell_id[0]);
+
+		// Bob tries to get the deleted Task
+		const deletes: Array<SignedActionHashed<Delete>> = await toPromise(
+			bob.store.tasks.get(task.actionHash).deletes,
+		);
+		assert.equal(deletes.length, 1);
+	});
 });
