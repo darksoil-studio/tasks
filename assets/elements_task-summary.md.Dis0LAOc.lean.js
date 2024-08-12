ab0ceb0f0989107f20c8fee60bd834bac3055f54
guillemcordoba <guillemcordoba@users.noreply.github.com>
2024-08-12 10:38:41 +0000

    Deploying to gh-pages from @ darksoil-studio/tasks@18839e61bcd11fb07b1dbac0d02b59454bedfd92 🚀

---
+++ assets/elements_task-summary.md.Dis0LAOc.lean.js
@@ -0,0 +1,12 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/chunks/api-docs.6GVAIKQB.js","assets/chunks/api-viewer-tabs.bc9mZ4w5.js","assets/chunks/tslib.es6.kHcLnhpD.js","assets/chunks/api-demo.Bsqmng5d.js","assets/chunks/profiles-context.BuH_RMZO.js","assets/chunks/tasks-client.qAG7b6b9.js","assets/chunks/provide.-X3GMMG4.js","assets/chunks/property.QJJZFMkB.js","assets/chunks/signal-watcher.DC_yjXfn.js","assets/chunks/tasks-context.D8BqHQfx.js","assets/chunks/context.CVKhpfoq.js","assets/chunks/task-summary.BEiASs-o.js","assets/chunks/agent-avatar.DHVeJeSD.js","assets/chunks/chunk.BUIP557I.v6fbXhoA.js","assets/chunks/chunk.C2LF7HHG.DYJ60H94.js","assets/chunks/chunk.DUWACTPH.C7rGf8ye.js","assets/chunks/static.DYJOwAbU.js"])))=>i.map(i=>d[i]);
+import{y as k,X as s,o,c as r,a3 as d,j as c}from"./chunks/framework.D8cWuh-X.js";import{d as m,P as E,a as g,b as u,T as y,c as b,s as _,e as v,j as f,f as F}from"./chunks/tasks-client.qAG7b6b9.js";import{n as C,i as A}from"./chunks/static.DYJOwAbU.js";const w=d("",12),B=c("api-docs",{src:"custom-elements.json",only:"task-summary"},null,-1),P=[w,B],H=JSON.parse('{"title":"<task-summary>","description":"","frontmatter":{},"headers":[],"relativePath":"elements/task-summary.md","filePath":"elements/task-summary.md"}'),D={name:"elements/task-summary.md"},I=Object.assign(D,{setup(T){return k(async()=>{await s(()=>import("./chunks/api-docs.6GVAIKQB.js"),__vite__mapDeps([0,1,2])),await s(()=>import("./chunks/api-demo.Bsqmng5d.js"),__vite__mapDeps([3,1,2])),await s(()=>import("./chunks/profiles-context.BuH_RMZO.js"),__vite__mapDeps([4,2,5,6,7,8])),customElements.get("tasks-context")||await s(()=>import("./chunks/tasks-context.D8BqHQfx.js"),__vite__mapDeps([9,6,7,5,10])),customElements.get("task-summary")||await s(()=>import("./chunks/task-summary.BEiASs-o.js"),__vite__mapDeps([11,12,5,2,7,8,13,14,15,16,10]));const a=await m(),t=new E(a,Array.from(a.keys())[0]),l=new g(new u(t,"tasks_test")),i=new y,e=new b(i,"tasks_test"),n=await _(e),h=await i.create_task(n),p=new v(e);f(C`
+    <profiles-context .store=${l}>
+      <tasks-context .store=${p}>
+        <api-demo src="custom-elements.json" only="task-summary" exclude-knobs="store">
+          <template data-element="task-summary" data-target="host">
+            <task-summary task-hash="${A(F(h.signed_action.hashed.hash))}"></task-summary>
+          </template>
+        </api-demo>
+      </tasks-context>
+    </profiles-context>
+  `,document.querySelector("element-demo"))}),(a,t)=>(o(),r("div",null,P))}});export{H as __pageData,I as default};
--- backend/release/build/cranelift-codegen-35c6824c7db13aec/out/version.rs
+++ backend/release/build/cranelift-codegen-35c6824c7db13aec/out/version.rs
@@ -1,2 +1,2 @@
 /// Version number of this crate. 
-pub const VERSION: &str = "0.91.1-796ddc89c";
+pub const VERSION: &str = "0.91.1-18839e61b";
